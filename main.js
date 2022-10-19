f9ae400e552c1775c6427a99c2e43dc262901812
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2022-10-19 14:49:02 -0600

    vf

---
+++ main.js
@@ -0,0 +1,13 @@
+//array
+
+//global
+let myData = [200, 444, 220, 100, 700];
+
+//display
+drawarray();
+
+function drawarray() {
+  for (let i = 0; i < myData.length; i++) {
+    console.log(myData[i]);
+  }
+}
