6aea020a37fa97d8ab093019a2b10b37c8e34176
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2022-10-20 13:55:36 -0600

    dc

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 //array
 
+//HTML
+let containerE1 = document.getElementById("container")
 //global
-let myData = [200, 444, 220, 100, 700];
+let myData = [];
+for (let n=1; n<=20; n++){
+  myData.push(300);
+}
+
 
 //display
 drawarray();
 
 function drawarray() {
+  let outputstr = "";
   for (let i = 0; i < myData.length; i++) {
     console.log(myData[i]);
+    outputstr += `<div style="height: ${myData[i]}px"></div>`
   }
+  containerE1.innerHTML = outputstr;
 }
