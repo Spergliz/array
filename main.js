8fa97b56796315f8801bba2a5d1c0411a95bdbbf
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2022-10-25 14:43:37 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 //array
 
 //HTML
-let containerE1 = document.getElementById("container")
+let containerE1 = document.getElementById("container");
 //global
 let myData = [];
-for (let n=1; n<=20; n++){
-  myData.push(Math.random()*600);
+for (let n = 1; n <= 5; n++) {
+  myData.push(Math.random() * 600);
 }
-
 
 //display
 drawarray();
@@ -15,16 +14,19 @@ drawarray();
 function drawarray() {
   let outputstr = "";
   for (let i = 0; i < myData.length; i++) {
-    console.log(myData[i]);
-    outputstr += `<div style="height: ${myData[i]}px"></div>`
+    outputstr += `<div style="height: ${myData[i]}px"></div>`;
   }
   containerE1.innerHTML = outputstr;
 }
-
 //key
-document,addEventListener("keydown",keydownhandler);
-function keydownhandler(event){
-  for (let i = 0; i < myData.length; i++) {
-  myData[0] += 600;
-}
+document, addEventListener("keydown", keydownhandler);
+function keydownhandler(event) {
+  if (event.keyCode === 38) {
+    myData.push(Math.random() * 600);
+  } else if (event.keyCode === 40) {
+   myData.pop()
+    
+  }
+  //redraw
+  drawarray();
 }
